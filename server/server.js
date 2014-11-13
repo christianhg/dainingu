aa97a94a41c9cd5e8e56350466affb296e3f3698
christianhg <christianhg@gmail.com>
2014-11-13 20:01:10 +0100

    Added express server with socket.io integration

---
+++ server/server.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    var express = require('express');
+    var http = require('http');
+    var mongoose = require('mongoose');
+    var morgan = require('morgan');
+
+    var app = express();
+    var server = app.listen(2000);
+    var io = require('socket.io').listen(server);
+
+    mongoose.connect('mongodb://localhost/dainingu');
+
+    app.route('/')
+        .get(function(req, res) {
+            res.sendFile(__dirname + '/index.html');
+        });
+
+    app.use(morgan('dev'));
+
+    io.sockets.on('connection', function(socket) {
+        console.log('A new user connected!');
+        socket.emit('info', {msg: 'hello client'});
+    });
+})();
