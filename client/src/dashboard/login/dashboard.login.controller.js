276a1cfb9181b0f4471cc151a0edca9bbb715b7c
Christian Grøngaard <christianhg@gmail.com>
2014-11-18 18:50:28 +0100

    Cleaned dashboard.login.controller

--- client/src/dashboard/login/dashboard.login.controller.js
+++ client/src/dashboard/login/dashboard.login.controller.js
@@ -9,36 +9,18 @@
         var vm = this;
 
         vm.dashboardLogin = function(loginData) {
-            /*auth.save(loginData, function(data) {
-                if(data.success) {
-                    // Get generated JWT token.
-                    var token = data.token;
-                    // Store token in sessionStorage.
-                    $window.sessionStorage.token = token;
-                    // Initiate socket
-                    socket.init();
-                } else {
-                    // Something went wrong. Delete token from sessionStorage if it exists.
-                    delete $window.sessionStorage.token;
-                }
-            });*/
-
             auth.signin(loginData, function(data) {
                 console.log(data);
                 if(data.success) {
                     // Get generated JWT token.
                     var token = data.token;
-                    var user = data.user;
                     // Store token in sessionStorage.
                     $window.sessionStorage.token = token;
-
-                    $window.sessionStorage.username = user.username;
                     // Initiate socket
                     socket.init();
                 } else {
                     // Something went wrong. Delete token from sessionStorage if it exists.
                     delete $window.sessionStorage.token;
-                    delete $window.sessionStorage.user;
                 }
             });
         };
