fbd9d9cc3c25150992dae55a5e67b4a2b999185e
Christian Grøngaard <christianhg@gmail.com>
2014-11-19 12:58:52 +0100

    Added validation on activateMenucardForm

--- client/src/menucard/activate/menucard.activate.controller.js
+++ client/src/menucard/activate/menucard.activate.controller.js
@@ -8,16 +8,26 @@
     function MenucardActivateController(auth, $window) {
         var vm = this;
 
-        vm.activateSession = function(key) {
+        vm.activateMenucard = function(key) {
+
             auth.activateSession(key, function(data) {
                 console.log(data);
+
+                vm.activationMessage = data.message;
+
+                // If activation was successful.
                 if(data.success) {
+                    vm.activationSuccessful = true;
+                    // Save token in sessionStorage.
                     var token = data.token;
                     $window.sessionStorage.menucardToken = token;
                 } else {
+                    vm.activationSuccessful = false;
+                    // Delete token from sessionStorage.
                     delete $window.sessionStorage.menucardToken;
                 }
             });
+            
         };
     }
 })();
