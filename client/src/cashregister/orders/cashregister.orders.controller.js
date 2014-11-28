5c2fb2f2f6090f3e103d510cac6a8fe850077f8b
christianhg <christianhg@gmail.com>
2014-11-28 12:05:34 +0100

    Implementing orders view in cash register

--- client/src/cashregister/orders/cashregister.orders.controller.js
+++ client/src/cashregister/orders/cashregister.orders.controller.js
@@ -5,7 +5,19 @@
         .module('dainingu.cashregister.orders')
         .controller('CashregisterOrdersController', CashregisterOrdersController);
 
-    function CashregisterOrdersController() {
+    function CashregisterOrdersController(sessions, socket) {
         var vm = this;
+
+        vm.getSessions = function() {
+            sessions.query(function(sessions) {
+               vm.sessions = sessions;
+            });
+        };
+
+        vm.getSessions();
+
+        socket.on('sessionsUpdated', function() {
+            vm.getSessions();
+        });
     }
 })();
