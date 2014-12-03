5f2868d58569add3e7977d69befa9963f8b9de00
Christian Grøngaard <christianhg@gmail.com>
2014-12-03 20:37:34 +0100

    Moved session and order directive from dashboard

--- client/src/dashboard/sessions/dashboard.sessions.controller.js
+++ client/src/dashboard/sessions/dashboard.sessions.controller.js
@@ -5,7 +5,7 @@
         .module('dainingu.dashboard.sessions')
         .controller('DashboardSessionsController', DashboardSessionsController);
 
-    function DashboardSessionsController(sessions, socket) {
+    function DashboardSessionsController(sessions, sessionsActivate, sessionsExpire, sessionsOrders, sessionsOrdersCommit, sessionsOrdersConfirm, sessionsOrdersComplete, sessionsOrdersClose, sessionsOrdersDishes, socket) {
         var vm = this;
 
         vm.getSessions = function() {
@@ -24,5 +24,94 @@
             vm.getSessions();
         });
 
+        vm.activateSession = function(sessionId) {
+            sessionsActivate.activate({sessionId: sessionId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.deactivateSession = function(sessionId) {
+            sessionsActivate.deactivate({sessionId: sessionId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.expireSession = function(sessionId) {
+            sessionsExpire.expire({sessionId: sessionId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.resumeSession = function(sessionId) {
+            sessionsExpire.resume({sessionId: sessionId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.deleteSession = function(id) {
+            sessions.delete({}, {'id': id}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.deleteOrder = function(sessionId, orderId) {
+            sessionsOrders.delete({sessionId: sessionId, orderId: orderId}, function(data) {
+
+            });
+        };
+
+        vm.removeDishFromOrder = function(sessionId, orderId, dishId) {
+            sessionsOrdersDishes.delete({sessionId: sessionId, orderId: orderId, dishId: dishId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.commitOrder = function(sessionId, orderId) {
+            sessionsOrdersCommit.commit({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.pullOrder = function(sessionId, orderId) {
+            sessionsOrdersCommit.pull({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.confirmOrder = function(sessionId, orderId) {
+            sessionsOrdersConfirm.confirm({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.rejectOrder = function(sessionId, orderId) {
+            sessionsOrdersConfirm.reject({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.completeOrder = function(sessionId, orderId) {
+            sessionsOrdersComplete.complete({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.incompleteOrder = function(sessionId, orderId) {
+            sessionsOrdersComplete.incomplete({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.closeOrder = function(sessionId, orderId) {
+            sessionsOrdersClose.close({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
+
+        vm.openOrder = function(sessionId, orderId) {
+            sessionsOrdersClose.open({sessionId: sessionId, orderId: orderId}, function(data) {
+                console.log(data);
+            });
+        };
     }
 })();
